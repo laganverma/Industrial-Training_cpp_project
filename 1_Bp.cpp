4517e4f53507292bb895c34ab800a78f0af1a84f
LAGAN <laganverma068@gmail.com>
2021-06-24 14:30:58 +0530

    24/06/2021_3 1.updated 1_Bp.cpp

--- 1_Bp.cpp
+++ 1_Bp.cpp
@@ -1,4 +1,27 @@
-//
-// Created by LAGAN on 24-06-2021.
-//
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    float a, b;
+
+    cout << "ENTER YOUR HIGH BP VALUE'S" << endl;
+    cin >> a;
+    cout << "ENTER YOUR LOW BP VALUE'S" << endl;
+    cin >> b;
+    if (a >= 120 && a <= 140) {
+        cout << "YOUR BP IS IN NORMAL RANGE" << endl;
+    } else if (a < 120) {
+        cout << "YOUR BP IS LOW" << endl;
+    } else {
+        cout << "YOUR BP IS HIGH" << endl;
+    }
+    if (b >= 120 && b <= 140) {
+        cout << "YOUR BP IS IN NORMAL RANGE" << endl;
+    } else if (b < 120) {
+        cout << "YOUR BP IS LOW" << endl;
+    } else {
+        cout << "YOUR BP IS HIGH" << endl;
+    }
+    return 0;
+}
