0a539903626e6b7036a53d9a630e7dc3446f68f1
LAGAN <laganverma068@gmail.com>
2021-06-24 23:33:36 +0530

    24/06/2021_3 1.updated 1_Bp.cpp (added a class)

--- 1_Bp.cpp
+++ 1_Bp.cpp
@@ -2,26 +2,54 @@
 
 using namespace std;
 
-int main() {
+class BP {
     float a, b;
+public:
+    void Input() {
+        cout << "ENTER YOUR HIGH BP VALUE'S = ";
+        cin >> a;
+        cout << "\nENTER YOUR LOW BP VALUE'S = ";
+        cin >> b;
+    }
 
-    cout << "ENTER YOUR HIGH BP VALUE'S" << endl;
-    cin >> a;
-    cout << "ENTER YOUR LOW BP VALUE'S" << endl;
-    cin >> b;
-    if (a >= 120 && a <= 140) {
-        cout << "YOUR BP IS IN NORMAL RANGE" << endl;
-    } else if (a < 120) {
-        cout << "YOUR BP IS LOW" << endl;
-    } else {
-        cout << "YOUR BP IS HIGH" << endl;
+    void High() {
+        if (a < 120) {
+            cout << "YOUR BP IS IN NORMAL RANGE(HIGH)" << endl;
+        } else if (a >= 120 && a <= 129) {
+            cout << "YOUR BP IS ELEVATED(HIGH)" << endl;
+        } else if (a >= 130 && a <= 139) {
+            cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 1)(HIGH)" << endl;
+        } else if (a >= 140 && a <= 179) {
+            cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 2)(HIGH)" << endl;
+        } else if (a >= 180) {
+            cout << "YOUR BP IS VERY HIGH(HIGH)" << endl;
+        }
     }
-    if (b >= 120 && b <= 140) {
-        cout << "YOUR BP IS IN NORMAL RANGE" << endl;
-    } else if (b < 120) {
-        cout << "YOUR BP IS LOW" << endl;
-    } else {
-        cout << "YOUR BP IS HIGH" << endl;
+
+    void Low() {
+        if (b < 79) {
+            cout << "YOUR BP IS IN NORMAL RANGE(LOW)" << endl;
+        } else if (b < 80) {
+            cout << "YOUR BP IS ELEVATED(LOW)" << endl;
+        } else if (b >= 80 && b <= 89) {
+            cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 1)(LOW)" << endl;
+        } else if (b >= 90 && b <= 119) {
+            cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 2)(LOW)" << endl;
+        } else if (b >= 120) {
+            cout << "YOUR BP IS VERY HIGH(LOW)" << endl;
+        }
     }
+};
+
+int main() {
+
+    BP user_1;
+    user_1.Input();
+    user_1.High();
+    user_1.Low();
     return 0;
 }
+
+
+
+
