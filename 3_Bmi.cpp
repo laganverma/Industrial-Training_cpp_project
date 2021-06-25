e0b9635db411e7369eb138da134cfbde403fde6f
Lagan <laganverma068@gmail.com>
2021-06-26 01:08:50 +0530

    26/06/2021_2 1.updated bmi , added switch (create 4 cases)

--- 3_Bmi.cpp
+++ 3_Bmi.cpp
@@ -1,22 +1,59 @@
 #include<iostream>
+
 using namespace std;
 
-int main()
-{
-    float bmi,w,h; //w=weight , h=height
+int main() {
+    float bmi, w, h;
+    int s;
 
-    cout<<"Please Enter your weight in \n1.Pound(lbs) \n 2.Kilogram(Kg) : ";
-    cin>>w;
-    cout<<"Please Enter your Height in Inches : ";
-    cin>>h;
-    bmi=(w*703)/(h*h);
-    cout<<"Your BMI is : "<<bmi<<endl;
+    cout << "Please Enter your weight in \n1.Pound(lbs) \n 2.Kilogram(Kg) : ";
+    cin >> s;
 
-    if(bmi>25)
-        cout<<"Overweight";
-    else if(bmi<25 && bmi>18.5)
-        cout<<"Optimal";
-    else
-        cout<<"Underweight";
-    return 0;
-}// added
+
+    switch (s) {
+        case 1: //pound and inches
+            cout << "Please Enter your Weight in Pounds : ";
+            cin >> w;
+            cout << "Please Enter your Height in Inches : ";
+            cin >> h;
+            bmi = (w * 703) / (h * h);
+            cout << "Your BMI is : " << bmi << endl;
+            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*    if (bmi > 25)
+                    cout << "Overweight";
+                else if (bmi < 25 && bmi > 18.5)
+                    cout << "Optimal";
+                else
+                    cout << "Underweight";
+                return 0;
+        }*/
+    }
+}
