d36b660a91126441909df02fe7cdf802eaf94f15
LAGAN <laganverma068@gmail.com>
2021-06-26 22:14:59 +0530

    26/06/2021_3 1. updated 3_Bmi.cpp

--- 3_Bmi.cpp
+++ 3_Bmi.cpp
@@ -1,40 +1,36 @@
 #include<iostream>
 #include <iomanip>
 using namespace std;
-
 int main() {
     float bmi, w, h;
     int s;
-
-    cout << "Please Enter your weight in \n1.Pound(lbs) \n 2.Kilogram(Kg) : ";
+    cout << "Please Enter your weight in \n1.Kilogram(Kg) \n2.Pound(lbs) " << endl;
     cin >> s;
-
-
-//    switch (s) {
-//    case 1: //pound and inches
-    cout << "Please Enter your Weight in Kg : ";
-    cin >> w;
-    cout << "Please Enter your Height in Cm : ";
-    cin >> h;
-    bmi = (w) / (h * h) * 10000;
-
-    cout << "Your BMI is : " <<setprecision(4)<< bmi << endl;
-    // break;
-
-
-
-    {
-        if (bmi > 25) {
-            cout << "Overweight";
-        } else if (bmi < 25 && bmi > 18.5) {
-            cout << "Optimal";
-        } else {
-            cout << "Underweight";
-        }
+    switch (s) {
+        case 1: //Kg and cm (metric)
+            cout << "Please Enter your Weight in Kg : ";
+            cin >> w;
+            cout << "Please Enter your Height in Cm : ";
+            cin >> h;
+            bmi = (w) / (h * h) * 10000;
+            cout << "Your BMI is : " << setprecision(4) << bmi << endl;
+            break;
+        case 2: //Pound and Inches (english)
+            cout << "Please Enter your weight in Pounds : ";
+            cin >> w;
+            cout << "Please Enter your Height in Inches : ";
+            cin >> h;
+            bmi = (w * 703) / (h * h);
+            cout << "Your BMI is : " << setprecision(4) << bmi << endl;
+            break;
     }
-
-
-    return 0;
-
-
+    if (bmi > 25) {
+        cout << "Overweight";
+    } else if (bmi < 25 && bmi > 18.5) {
+        cout << "Optimal";
+    } else {
+        cout << "Underweight";
+    }
+        return 0;
 }
+
