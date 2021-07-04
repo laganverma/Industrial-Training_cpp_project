d2bd78e4be77a6aa77acb92b1ae38e4925b1ac77
LAGAN <laganverma068@gmail.com>
2021-07-04 23:53:03 +0530

    04/07/2021_3 1.updated

--- main_practice.cpp
+++ main_practice.cpp
@@ -42,7 +42,7 @@ int main() {
     gotoxy(25, 7);
     cout << "2.OXYGEN CALCULATOR";
     gotoxy(25, 8);
-    cout << "3.BMI";
+    cout << "3.BMI(BODY MASS INDEX) - CALCULATOR";
     gotoxy(25, 9);
     cout << "4.COVID SYMPTOMS";
     gotoxy(25, 10);
@@ -68,125 +68,125 @@ int main() {
         case 3:
             gotoxy(25, 17);
             cout << "---------------------------------------------------------------------------";
-            gotoxy(60, 18);
-            cout << "BMI";
+            gotoxy(45, 18);
+            cout << "BMI (BODY MASS INDEX) - CALCULATOR";
             gotoxy(25, 19);
             cout << "---------------------------------------------------------------------------";
             gotoxy(25, 21);
             cout << "******Welcome to Bmi Calculator******";
             gotoxy(25, 23);
-            cout << "Please Enter your weight in";
+            cout << "Enter your weight in : ";
             gotoxy(25, 24);
             cout << "1.Kilogram(Kg)";
             gotoxy(25, 25);
             cout << "2.Pound(lbs)";
-            gotoxy(25, 27);
-            cout << "Input = ";
+            gotoxy(25, 26);
+            cout << "Choice = ";
             cin >> s;
             switch (s) {
                 case 1:
-                    gotoxy(25, 29);
+                    gotoxy(25, 27);
                     cout << "Please Enter your Weight in Kg : ";
                     cin >> w;
-                    gotoxy(25, 30);
+                    gotoxy(25, 29);
                     cout << "Enter Your Height in";
-                    gotoxy(25, 31);
+                    gotoxy(25, 30);
                     cout << "1.Cm";
-                    gotoxy(25, 32);
+                    gotoxy(25, 31);
                     cout << "2.Inches";
-                    gotoxy(25, 34);
-                    cout << "Input = ";
+                    gotoxy(25, 32);
+                    cout << "Choice = ";
                     cin >> s_1;
                     switch (s_1) {
                         case 1://Kg and cm (metric)
-                            gotoxy(25, 36);
+                            gotoxy(25, 33);
                             cout << "Please Enter your Height in Cm : ";
                             cin >> h;
                             bmi = (w) / (h * h) * 10000;
-                            gotoxy(25, 37);
+                            gotoxy(25, 34);
                             cout << "Your BMI is : " << setprecision(4) << bmi << endl;
                             break;
                         case 2://Kg and Inches (Custom)
-                            gotoxy(25, 31);
+                            gotoxy(25, 33);
                             cout << "Please Enter your Height in Feet: ";
                             cin >> h;
-                            gotoxy(25, 32);
+                            gotoxy(25, 34);
                             cout << "Please Enter your Height in inches: ";
                             cin >> h_1;
                             convert_2 = (h * 12) + h_1;
                             convert_1 = w * 2.20462;
                             bmi = (convert_1 * 703) / (convert_2 * convert_2);
-                            gotoxy(25, 33);
+                            gotoxy(25, 35);
                             cout << "Your BMI is : " << setprecision(4) << bmi << endl;
                             break;
                     }
-
                     break;
                 case 2:
-                    gotoxy(25, 25);
+                    gotoxy(25, 27);
                     cout << "Please Enter your weight in Pounds : ";
                     cin >> w;
-                    gotoxy(25, 28);
-                    cout << "1.Cm";
                     gotoxy(25, 29);
-                    cout << "2.Inches";
+                    cout << "Enter Your Height in";
                     gotoxy(25, 30);
+                    cout << "1.Cm";
+                    gotoxy(25, 31);
+                    cout << "2.Inches";
+                    gotoxy(25, 32);
                     cout << "Input = ";
                     cin >> s_2;
                     switch (s_2) {
                         case 1://Pound and Cm (Custom)
-                            gotoxy(25, 31);
+                            gotoxy(25, 33);
                             cout << "Please Enter your Height in Cm : ";
                             cin >> h;
                             convert_3 = w / 2.20462;
                             bmi = (convert_3) / (h * h) * 10000;
-                            gotoxy(25, 33);
+                            gotoxy(25, 34);
                             cout << "Your BMI is : " << setprecision(4) << bmi << endl;
                             break;
                         case 2://Pound and Inches (english)
-                            gotoxy(25, 31);
+                            gotoxy(25, 33);
                             cout << "Please Enter your Height in Feet: ";
                             cin >> h;
-                            gotoxy(25, 32);
+                            gotoxy(25, 34);
                             cout << "Please Enter your Height in inches: ";
                             cin >> h_1;
                             convert_2 = (h * 12) + h_1;
                             bmi = (w * 703) / (convert_2 * convert_2);
-                            gotoxy(25, 33);
+                            gotoxy(25, 35);
                             cout << "Your BMI is : " << setprecision(4) << bmi << endl;
                             break;
                     }
                     break;
             }
             if (bmi > 25) {
-                gotoxy(25, 35);
+                gotoxy(25, 37);
                 cout << "_Overweight_";
                 int a;
-                gotoxy(25, 36);
+                gotoxy(25, 39);
                 cout << "IF YOU WANT A DIET PLAN PLZ REFER TO OPTIONS DOWN  BELOW";
-                gotoxy(25, 37);
+                gotoxy(25, 40);
                 cout << "1. DIET PLAN";
-                gotoxy(25, 38);
+                gotoxy(25, 41);
                 cout << "2. EXIT";
                 cin >> a;
                 switch (a) {
                     case 1:
-                        gotoxy(25, 40);
+                        gotoxy(25, 43);
                         cout << "DIET PLAN";
-                        gotoxy(25, 41);
+                        gotoxy(25, 44);
                         cout << "1.Vegetarian";
-                        gotoxy(25, 42);
+                        gotoxy(25, 45);
                         cout << "2.Non-Vegetarian";
                         int veg;
                         cin >> veg;
                         if (veg == 1) {
-                            gotoxy(25, 45);
+                            gotoxy(25, 46);
                             cout << "Veg food";
                         } else {
-                            gotoxy(25, 45);
+                            gotoxy(25, 46);
                             cout << "non veg food";
                         }
-
 
                         break;
                     case 2:
@@ -198,31 +198,31 @@ int main() {
                 gotoxy(25, 35);
                 cout << "_Optimal_";
             } else {
-                gotoxy(25, 35);
+                gotoxy(25, 37);
                 cout << "_Underweight_";
                 int a;
-                gotoxy(25, 36);
+                gotoxy(25, 39);
                 cout << "IF YOU WANT A DIET PLAN PLZ REFER TO OPTIONS DOWN  BELOW";
-                gotoxy(25, 37);
+                gotoxy(25, 40);
                 cout << "1. DIET PLAN";
-                gotoxy(25, 38);
+                gotoxy(25, 41);
                 cout << "2. EXIT";
                 cin >> a;
                 switch (a) {
                     case 1:
-                        gotoxy(25, 40);
+                        gotoxy(25, 43);
                         cout << "DIET PLAN";
-                        gotoxy(25, 41);
+                        gotoxy(25, 44);
                         cout << "1.Vegetarian";
-                        gotoxy(25, 42);
+                        gotoxy(25, 45);
                         cout << "2.Non-Vegetarian";
                         int veg;
                         cin >> veg;
                         if (veg == 1) {
-                            gotoxy(25, 45);
+                            gotoxy(25, 46);
                             cout << "Veg food";
                         } else {
-                            gotoxy(25, 45);
+                            gotoxy(25, 473);
                             cout << "non veg food";
                         }
                 }
