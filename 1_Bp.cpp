2d77e270eca08cc2339cb4065e3df0189c32b063
Lagan <laganverma068@gmail.com>
2021-07-06 10:56:32 +0530

    06/07/2021_1 1.added text

--- 1_Bp.cpp
+++ 1_Bp.cpp
@@ -3,39 +3,40 @@
 using namespace std;
 
 class BP {
-    float a, b;
+    long sys, dia;
 public:
     void Input() {
         cout << "ENTER SYSTOLIC PRESSURE VALUES (TOP NUMBER) = ";
-        cin >> a;
+        cin >> sys;
         cout << "\nENTER DIASTOLIC PRESSURE VALUES (BOTTOM NUMBER) = ";
-        cin >> b;
+        cin >> dia;
     }
 
     void High() const {
-        if (a < 120) {
+        if (sys < 120) {
             cout << "YOUR BP IS IN NORMAL RANGE(HIGH)" << endl;
-        } else if (a >= 120 && a <= 129) {
+        } else if (sys >= 120 && sys <= 129) {
             cout << "YOUR BP IS ELEVATED(HIGH)" << endl;
-        } else if (a >= 130 && a <= 139) {
+        } else if (sys >= 130 && sys <= 139) {
             cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 1)(HIGH)" << endl;
-        } else if (a >= 140 && a <= 179) {
+        } else if (sys >= 140 && sys <= 179) {
             cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 2)(HIGH)" << endl;
-        } else if (a >= 180) {
+        } else if (sys >= 180) {
             cout << "YOUR BP IS VERY HIGH(HIGH)" << endl;
         }
     }
 
     void Low() const {
-        if (b < 79) {
+        if (dia < 79) {
             cout << "YOUR BP IS IN NORMAL RANGE(LOW)" << endl;
-        } else if (b < 80) {
+        } else if (dia < 80) {
             cout << "YOUR BP IS ELEVATED(LOW)" << endl;
-        } else if (b >= 80 && b <= 89) {
+        } else if (dia >= 80 && dia <= 89) {
             cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 1)(LOW)" << endl;
-        } else if (b >= 90 && b <= 119) {
+        } else if (dia >= 90 && dia <= 119) {
             cout << "YOUR BP IS HIGH (HYPERTENSION STAGE 2)(LOW)" << endl;
-        } else if (b >= 120) {
+        } else if (dia
+                   >= 120) {
             cout << "YOUR BP IS VERY HIGH(LOW)" << endl;
         }
     }
@@ -43,10 +44,10 @@ public:
 
 int main() {
 
-    BP user_1;
-    user_1.Input();
-    user_1.High();
-    user_1.Low();
+    BP user{};
+    user.Input();
+    user.High();
+    user.Low();
     return 0;
 }
 
