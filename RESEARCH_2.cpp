d0bbc35b0da57bc8878053a8bbc8f5f032667a60
LAGAN <laganverma068@gmail.com>
2021-07-11 14:28:32 +0530

    11/07/2021_6 1.updated

---
+++ RESEARCH_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << endl << endl << endl << endl;
+    cout << "\tB.M.I" << "\t\t\t\t" << "WEIGHT STATUS" << endl << endl;
+    cout << "\tBELOW 18.5" << "\t\t\t" << "UNDER_WEIGHT" << endl;
+    cout << "\t18.5 - 24.9" << "\t\t\t" << "NORMAL OR HEALTHY_WEIGHT" << endl;
+    cout << "\t25.0 - 29.9" << "\t\t\t" << "OVER_WEIGHT" << endl;
+    cout << "\t30.0 AND ABOVE" << "\t\t\t" << "OBESE" << endl << endl;
+}
