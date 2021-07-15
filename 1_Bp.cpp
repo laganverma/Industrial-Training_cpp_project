21f11b61281b884468e80c9c71c89e8880cd28fa
LAGAN <laganverma068@gmail.com>
2021-07-16 01:35:10 +0530

    16/07/2021_8 1.updated

--- 1_Bp.cpp
+++ 1_Bp.cpp
@@ -25,6 +25,20 @@ public:
             cout << "YOUR BP IS VERY HIGH" << endl;
         }
     }
+
+    void Facts() {
+        cout << "\n\n\nWhy High Blood Pressure is a \"Silent Killer\"?";
+        cout << "\n\n\n Most of the time, high blood pressure (HBP, or hypertension) has no obvious symptoms \n"
+                "to indicate that something is wrong. The best ways to protect yourself are being aware of \n "
+                "the risks and making changes that matter.\n"
+                "\n"
+                "         A few facts to be aware of:\n"
+                "* Many people with high blood pressure don’t even know they have it. Often the signs and symptoms are misunderstood.\n"
+                "* High blood pressure develops slowly over time and can be related to many causes.\n"
+                "* High blood pressure cannot be cured. But it can be managed effectively through \n"
+                "lifestyle changes and, when needed, medication.";
+
+    }
 };
 
 
@@ -32,6 +46,7 @@ int main() {
     BP user{};
     user.Input();
     user.High();
+    user.Facts();
     return 0;
 }
 
