667a169dfed789bb73248d21a2e7a2706187faff
LAGAN <laganverma068@gmail.com>
2021-07-22 18:02:35 +0530

    21/07/2021_8 1.updated

--- main_practice.cpp
+++ main_practice.cpp
@@ -593,35 +593,73 @@ int main() {
         case 4:
             gotoxy(25, 17);
             cout << "---------------------------------------------------------------------------";
-            gotoxy(50,18);
+            gotoxy(52, 18);
             cout << "COVID-19 SYMPTOMS";
-            gotoxy(25,19);
+            gotoxy(25, 19);
             cout << "---------------------------------------------------------------------------";
-            gotoxy(25, 21);
-            cout << "\n\t\t\tMost common symptoms:";
-            cout << "\n\t\t\t . FEVER";
-            cout << "\n\t\t\t . DRY COUGH";
-            cout << "\n\t\t\t . TIREDNESS";
-            cout << "\n\n\t\t\tLESS COMMON SYMPTOMS:";
-            cout << "\n\t\t\t . ACHES AND PAINS";
-            cout << "\n\t\t\t . SORE THROAT";
-            cout << "\n\t\t\t . DIARRHOEA";
-            cout << "\n\t\t\t . CONJUNCTIVITIS";
-            cout << "\n\t\t\t . HEADACHE";
-            cout << "\n\t\t\t . LOSS OF TASTE AND OR SMELL";
-            cout << "\n\t\t\t . A RASH ON SKIN, OR DISCOLOURATION OF FINGERS OR TOES ";
-            cout << "\n\n\t\t\tSERIOUS SYMPTOMS:";
-            cout << "\n\t\t\t . DIFFICULTY BREATHING OR SHORTNESS OF BREATH";
-            cout << "\n\t\t\t . CHEST PAIN OR PRESSURE";
-            cout << "\n\t\t\t . LOSS OF SPEECH OR MOVEMENT";
-            int n;
-            cout << "value=\n";
-            cin >> n;
-            if (n == 1)
-                goto label1;
-            else
-                exit(0);
-            break;
+            gotoxy(25, 22);
+            cout << "-- Most common symptoms:";
+            gotoxy(25, 23);
+            cout << "   * FEVER";
+            gotoxy(25, 24);
+            cout << "   * DRY COUGH";
+            gotoxy(25, 25);
+            cout << "   * TIREDNESS";
+            gotoxy(25, 27);
+            cout << "-- LESS COMMON SYMPTOMS:";
+            gotoxy(25, 28);
+            cout << "   * ACHES AND PAINS";
+            gotoxy(25, 29);
+            cout << "   * SORE THROAT";
+            gotoxy(25, 30);
+            cout << "   * DIARRHOEA";
+            gotoxy(25, 31);
+            cout << "   * CONJUNCTIVITIS";
+            gotoxy(25, 32);
+            cout << "   * HEADACHE";
+            gotoxy(25, 33);
+            cout << "   * LOSS OF TASTE AND OR SMELL";
+            gotoxy(25, 34);
+            cout << "   * A RASH ON SKIN, OR DISCOLOURATION OF FINGERS OR TOES ";
+            gotoxy(25, 36);
+            cout << "-- SERIOUS SYMPTOMS:";
+            gotoxy(25, 37);
+            cout << "   * DIFFICULTY BREATHING OR SHORTNESS OF BREATH";
+            gotoxy(25, 38);
+            cout << "   * CHEST PAIN OR PRESSURE";
+            gotoxy(25, 39);
+            cout << "   * LOSS OF SPEECH OR MOVEMENT";
+            gotoxy(25, 43);
+            cout << "Seek immediate medical attention if you have serious symptoms.Always call before visiting your ";
+            gotoxy(25, 44);
+            cout << "doctor or health facility.";
+            gotoxy(25, 46);
+            cout << "People with mild symptoms who are otherwise healthy should manage their symptoms at home.";
+            gotoxy(25, 48);
+            cout << "On average it takes 5 to 6 days from when someone is infected with the virus for symptoms to";
+            gotoxy(25, 49);
+            cout << "show,4 however it can take up to 14 days.";
+            gotoxy(25, 52);
+        tryy:
+            cout << "IF YOU WANT TO LOCATE A COVID-19 HOSPITAL NEAR YOU I CAN HELP YOU.(Y/N)";
+            gotoxy(25, 53);
+            cout << "1. YES";
+            gotoxy(25, 54);
+            cout << "2. NO";
+            gotoxy(25, 55);
+            cout << "3. EXIT";
+            gotoxy(25, 57);
+            cout << "CHOICE = ";
+            int yes_no;
+            switch (yes_no) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+
         case 5:
         label1:
             int states;
@@ -636,7 +674,7 @@ int main() {
             int Rajasthan;
 
         hospital_rerun:
-            cout << "\n\n\n\n";
+            cout << "\n\n";
             cout << "\n\t\t\t";
             cout << "---------------------------------------------------------------------------";
             cout << "\n\t\t\t\t\t\t";
@@ -1246,7 +1284,8 @@ int main() {
                             cout << "\n\n\t\t\t";
                             cout << "(ii) Hospital - Shalimar hospital ";
                             cout << "\n\t\t\t";
-                            cout<<  "     Address  - Block A A-989 Captain Aditya Mishra Marg, near Shalimar Chauraha, Lucknow,";
+                            cout
+                                    << "     Address  - Block A A-989 Captain Aditya Mishra Marg, near Shalimar Chauraha, Lucknow,";
                             cout << "\n\t\t\t";
                             cout << "                Uttar Pradesh 226016";
                             cout << "\n\t\t\t";
